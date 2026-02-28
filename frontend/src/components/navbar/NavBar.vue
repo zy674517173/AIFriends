4abf2fdd45db27b77eacf9d058c1c2c3e44e28e8
zy674517173 <674517173@qq.com>
2026-02-28 12:10:24 +0800

    创建导航栏

---
+++ frontend/src/components/navbar/NavBar.vue
@@ -0,0 +1,81 @@
+<script setup>
+
+import MenuIcon from "@/components/navbar/icons/MenuIcon.vue";
+import HomepageIcon from "@/components/navbar/icons/HomepageIcon.vue";
+import FriendIcon from "@/components/navbar/icons/FriendIcon.vue";
+import CreateIcon from "@/components/navbar/icons/CreateIcon.vue";
+import SearchIcon from "@/components/navbar/icons/SearchIcon.vue";
+</script>
+
+<template>
+  <div class="drawer lg:drawer-open">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
+      <!-- Navbar -->
+      <nav class="navbar w-full bg-base-100 shadow-sm">
+        <div class="navbar-start">
+          <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
+          <!-- Sidebar toggle icon -->
+          <MenuIcon />
+          </label>
+          <div class="px-2 font-bold text-xl">AIFriends</div>
+        </div>
+
+        <div class="navbar-center w-4/5 max-w-180">
+          <div class="join w-4/5">
+            <input class="input join-item rounded-l-full w-4/5" placeholder="搜索你感兴趣的内容" />
+            <button class="btn join-item rounded-r-full gap-0">
+              <SearchIcon />
+              搜索
+            </button>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <button class="btn btn-ghost text-lg">登录</button>
+        </div>
+      </nav>
+      <!-- Page content here -->
+      <slot></slot>
+    </div>
+
+    <div class="drawer-side is-drawer-close:overflow-visible">
+      <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+      <div class="flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-16 is-drawer-open:w-54">
+        <!-- Sidebar content here -->
+        <ul class="menu w-full grow">
+          <!-- List item -->
+          <li>
+            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py-3" data-tip="首页">
+              <!-- Home icon -->
+              <HomepageIcon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">首页</span>
+            </button>
+          </li>
+
+          <!-- List item -->
+          <li>
+            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py-3" data-tip="好友">
+              <!-- Home icon -->
+              <FriendIcon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">好友</span>
+            </button>
+          </li>
+
+          <!-- List item -->
+          <li>
+            <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right py-3" data-tip="创作">
+              <!-- Home icon -->
+              <CreateIcon />
+              <span class="is-drawer-close:hidden text-base ml-2 whitespace-nowrap">创作</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'path'
@@ -11,6 +12,7 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(),
   ],
   build: {
     outDir: path.resolve(__dirname, '../backend/static/frontend'), // 打包到 Django static

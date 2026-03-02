d8c6663661d189dae11410828cd7f22254b6be54
zy674517173 <674517173@qq.com>
2026-03-02 23:34:23 +0800

    “实现数据库”

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from web.models.user import UserProfile
+
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)       # 逗号不要删！！！
---
+++ backend/web/models/user.py
@@ -0,0 +1,21 @@
+import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.timezone import now, localtime
+
+
+def photo_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4().hex[:10]}.{ext}'
+    return f'user/photos/{instance.user_id}_{filename}'
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    photo = models.ImageField(default='user/photos/default.png', upload_to='photo_upload_to')
+    profile = models.TextField(default='谢谢你的关注', max_length=500)
+    creat_time = models.DateTimeField(default=now)
+    update_time = models.DateTimeField(default=now)
+
+    def __str__(self):
+        return f'{self.user.username} - {localtime(self.creat_time).strftime("%Y-%m-%d %H:%M:%S")}'

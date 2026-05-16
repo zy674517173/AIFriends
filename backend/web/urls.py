98819f47726c0098a3c6c9c6b486d162d556c83b
zy674517173 <674517173@qq.com>
2026-05-16 23:28:48 +0800

    给上次的内容打上补丁

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from web.views.index import index
 from web.views.user.account.get_user_info import GetUserInfoView
 from web.views.user.account.login import LoginView
@@ -14,4 +14,6 @@ urlpatterns = [
     path('api/user/account/get_user_info/', GetUserInfoView.as_view(), name='get_user_info'),
 
     path('', index),
+    # 在前端任意路径下刷新时，django都自动路由到根路径下，剩下的路由交由前端处理。
+    re_path(r'^(?!media/|static/|assets/).*$', index)
 ]

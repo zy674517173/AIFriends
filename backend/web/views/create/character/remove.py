efc5d4353474c34ed8c470a5c2717afa30644307
zy674517173 <674517173@qq.com>
2026-05-21 23:30:44 +0800

    修复上节课的补丁

--- backend/web/views/create/character/remove.py
+++ backend/web/views/create/character/remove.py
@@ -11,6 +11,9 @@ class RemoveCharacterView(APIView):
         try:
             character_id = request.data['character_id']
             Character.objects.filter(pk=character_id, auhtor__user=request.user).delete()
+            return Response({
+                'result': 'success',
+            })
         except:
             return Response({
                 "result": "系统异常，请稍后重试",
